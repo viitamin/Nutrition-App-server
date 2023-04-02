6d2a8b143755f8fdea9e233814fc45b8529af35b
viitamin <tmdals0708@gmail.com>
2023-04-02 16:23:24 +0900

    Feat project 계층 설정

--- config/express.js
+++ config/express.js
@@ -1,7 +1,12 @@
 import express from "express";
+import logger from "morgan";
+import userRouter from "../src/router/userRouter";
 
 const app = new express();
 
+app.use(logger("dev"));
 app.use(express.json());
+
+app.use("/users", userRouter);
 
 export default app;
